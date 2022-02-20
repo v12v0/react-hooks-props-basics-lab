ac0c1ff4f30ce4f720cff4d8f342ab0efb61bff6
Vi Vo <aaronvo118@gmail.com>
2022-02-20 10:51:59 -0600

    added links component

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ function About() {
       <h2>About Me</h2>
       <p>Put the bio in here</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <links github={props.links.github} linkedin={props.links.linkedin}/>
     </div>
   );
 }
